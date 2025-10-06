a1b552270fd2c52178864caad1dfbfef5719be05
noveres <andrew901117@gmail.com>
2025-10-06 17:24:59 +0800

    backend init

---
+++ backend/SpeakBook_Backend/src/main/java/com/example/speakbook_backend/SpeakBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.speakbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakBookBackendApplication.class, args);
+    }
+
+}
---
+++ backend/SpeakBook_Backend/src/test/java/com/example/speakbook_backend/SpeakBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speakbook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
