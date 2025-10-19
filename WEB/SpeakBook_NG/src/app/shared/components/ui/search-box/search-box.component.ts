13a973f99259a9d027b64f5487f1d5f00a76687b
noveres <andrew901117@gmail.com>
2025-10-20 00:22:55 +0800

    自動刷新機制，用戶不會刪除

--- WEB/SpeakBook_NG/src/app/shared/components/ui/search-box/search-box.component.ts
+++ WEB/SpeakBook_NG/src/app/shared/components/ui/search-box/search-box.component.ts
@@ -6,26 +6,39 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-search-box',
   imports: [CommonModule, FormsModule],
   templateUrl: './search-box.component.html',
-  styleUrl: './search-box.component.scss'
+  styleUrls: ['./search-box.component.scss']
 })
 export class SearchBoxComponent {
   @Input() placeholder: string = '搜尋...';
   @Input() searchValue: string = '';
+  @Input() autoSearchOnClear: boolean = true;  // 清空時是否自動搜尋
   @Output() searchValueChange = new EventEmitter<string>();
   @Output() search = new EventEmitter<string>();
+
+  private hasSearched: boolean = false;  // 追蹤是否已執行過搜尋
 
   onSearchChange(value: string): void {
     this.searchValue = value;
     this.searchValueChange.emit(value);
+    
+    // 如果已經搜尋過,且現在清空了文字,自動觸發搜尋
+    if (this.autoSearchOnClear && this.hasSearched && value.trim() === '') {
+      this.search.emit('');
+    }
   }
 
   onSearchClick(): void {
+    this.hasSearched = true;  // 標記已執行搜尋
     this.search.emit(this.searchValue);
   }
 
   clearSearch(): void {
     this.searchValue = '';
     this.searchValueChange.emit('');
-    this.search.emit('');
+    
+    // 點擊清除按鈕時,如果已經搜尋過且啟用自動搜尋,則發送空字串搜尋
+    if (this.autoSearchOnClear && this.hasSearched) {
+      this.search.emit('');
+    }
   }
 }
